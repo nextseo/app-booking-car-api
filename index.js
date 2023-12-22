76dbb56e122b8b4e940d0879245e9626e69d216a
nextseo <90128630+nextseo@users.noreply.github.com>
2023-12-22 22:52:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://app.xn--12cbx8beub8evezb2evdwa3gkk.com",
+      "https://app-booking-car-api.vercel.app"
     ],
     // methods: ["POST", "GET"],
     methods: ["POST, GET"],
